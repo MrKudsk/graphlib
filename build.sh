c0d774c9367ca96c50e16e6144ffbc5a7d36cabf
Kim Kudsk <kim.kudsk@gmail.com>
2024-09-22 21:28:27 +0200

    Init from new begning

--- build.sh
+++ build.sh
@@ -2,19 +2,4 @@
 
 set -xe
 
-COMMON_CFLAGS="-Wall -Wextra -ggdb -I. -I./build/ -I./thirdparty/"
-
-build_sdl_demo() {
-    NAME=$1
-    clang $COMMON_CFLAGS -O2 -o ./bin/demos/$NAME.sdl -DPLATFORM=SDL_PLATFORM ./demo/$NAME.c =lm =lSDL2
-}
-
-# Build test
-build_tests() {
-    mkdir -p ./build/
-    clang $COMMON_CFLAGS -o ./build/test test.c -lm
-}
-
-build_tests
-mkdir -p ./bin/demos
-build_sdl_demo triangle
+cc -Wall -Wextra -o example example.c 
